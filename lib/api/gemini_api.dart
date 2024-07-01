ba7b6383b44ee60a61d6fa33ca4dc13d9aba21a2
Saurabh Kumar Singh <73352093+Saurabh7Goku@users.noreply.github.com>
2024-07-02 05:22:04 +0530

    Update gemini_api.dart

--- lib/api/gemini_api.dart
+++ lib/api/gemini_api.dart
@@ -4,5 +4,93 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class GeminiAPI {
-  'Your Gemini Pro POST Request'
+  static Future<Map<String, String>> getHeader() async {
+    return {
+      'content-Type': 'application/json',
+    };
+  }
+
+  static Future<String> getGeminiData(BuildContext context, message) async {
+    try {
+      final header = await getHeader();
+
+      final Map<String, dynamic> requestBody = {
+        'contents': [
+          {
+            'parts': [
+              {
+                'text': '$message',
+              }
+            ]
+          }
+        ],
+        'generationConfig': {
+          'temperature': 0.8,
+          'maxOutputTokens': 1000 //The max return token as response.
+        },
+        "safetySettings": [
+          {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_ONLY_HIGH"
+          },
+          {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_ONLY_HIGH"
+          },
+          {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_ONLY_HIGH"
+          },
+          {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_ONLY_HIGH"
+          }
+        ]
+      };
+
+      String url =
+          'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyAfQbcjOLvntaFuqdmq4uz360RyL8QHFqg';
+
+      var response = await http.post(
+        Uri.parse(url),
+        headers: header,
+        body: jsonEncode(requestBody),
+      );
+
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+        return jsonResponse['candidates'][0]['content']['parts'][0]['text'];
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.red.shade800,
+            content: const Center(
+              child: Text(
+                'Nothing to be found of.',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            duration: const Duration(seconds: 5),
+          ),
+        );
+        return 'Nothing to be found of.';
+      }
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red.shade800,
+          content: Center(
+            child: Text(
+              'Check You Internet Connection! If not Working then Update the app to latest!!! ',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          duration: const Duration(seconds: 10),
+        ),
+      );
+      return "";
+    }
+  }
 }
+
